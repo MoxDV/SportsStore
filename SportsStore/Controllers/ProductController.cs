277ee84e97a3f468abf06096fa7e4b03da491bdb
moxdv777 <moxdv777@mail.ru>
2018-06-25 13:34:32 +0300

    Initialize start parametrs

---
+++ SportsStore/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+
+namespace SportsStore.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository _repository;
+
+        public ProductController(IProductRepository repository) {
+            _repository = repository;
+        }
+
+        public IActionResult List() => View(_repository.Products);
+    }
+}
---
+++ SportsStore/Models/FakeProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Models
+{
+    public class FakeProductRepository : IProductRepository
+    {
+        public IEnumerable<Product> Products => new List<Product> {
+            new Product{ Name = "Футбольный мяч", Price = 25},
+            new Product{ Name = "Доска для серфинга", Price = 179},
+            new Product{ Name = "Беговая обувь", Price = 95}
+        };
+    }
+}
